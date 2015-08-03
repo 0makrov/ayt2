f6dc296773bd5439c5bd888d38695718c77a958a
0makrov <0makrov@gmail.com>
2015-08-03 22:27:55 +0300

    init1

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup
 
 setup(
     name='pytest-ayt2',
-    py_modules=['pytest_ayt2'],
+    packages=['pytest_ayt2'],
     entry_points={'pytest11': ['ayt2 = pytest_ayt2']},
     platforms='any',
-    install_requires=['pytest']
+    install_requires=['pytest'],
 )
