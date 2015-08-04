073c582faba22ac0ab2bb922bf111ab8d70d38be
0makrov <0makrov@gmail.com>
2015-08-04 12:04:27 +0300

    t15

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from setuptools import setup
 
 setup(
     name='pytest-ayt2',
+    version = '14',
     packages=['pytest_ayt2'],
     entry_points={'pytest11': ['ayt2 = pytest_ayt2']},
     platforms='any',
--- tests/test_ayt2.py
+++ tests/test_ayt2.py
@@ -1,5 +1,7 @@
 import pytest
 
+import pytest_ayt2
+
 
 def func(x):
     return x + 1
@@ -9,3 +11,8 @@ def test_a():
     
 def test_b():
 	assert 4==4
+
+
+def test_version():
+    assert hasattr(pytest_ayt2, '__version__')
+    assert pytest_ayt2.'__version__' == '14'
