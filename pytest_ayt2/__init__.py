c1cebfeb95dc35af523dbe976c147efe4ee05845
0makrov <0makrov@gmail.com>
2015-08-04 11:54:48 +0300

    t13

--- pytest_ayt2/__init__.py
+++ pytest_ayt2/__init__.py
@@ -3,10 +3,3 @@ import pytest
 def pytest_addoption(parser):
     print('rrrr') #nothing doing
 
-    
-def tear_down():
-    print "\nTEARDOWN after all tests"
-    
-@pytest.fixture(scope="session", autouse=True)
-def set_up(request):
-    print "\nSETUP before all tests"
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,14 @@
 import pytest
 
 
+def tear_down():
+    print "\nTEARDOWN after all tests"
+    
+@pytest.fixture(scope="session", autouse=True)
+def set_up(request):
+    print "\nSETUP before all tests"
+
+
 
 
 def pytest_runtest_setup(item):
