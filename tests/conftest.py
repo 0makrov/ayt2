3a6124a07d2fbf7dc5400c9557f326b0a0e1b384
0makrov <0makrov@gmail.com>
2015-08-04 11:16:49 +0300

    tf

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
 import pytest
 
-print ('cccc')
+
+def tear_down():
+    print "\nTEARDOWN after all tests"
+    
+@pytest.fixture(scope="session", autouse=True)
+def set_up(request):
+    print "\nSETUP before all tests"
 
 def pytest_runtest_setup(item):
     print ("setting up", item)
